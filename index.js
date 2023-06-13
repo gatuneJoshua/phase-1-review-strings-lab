aa24ad9ec552a9002332bfc4317630c8a1a0d185
Joshua <joshua.gatune@student.moringaschool.com>
2023-06-13 11:23:11 +0300

    my first commit

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Write your code in this file!
+let currentUser = 'Josh';
+let welcomeMessage = "Welcome to Flatbook, ";
+currentUser = currentUser.toUpperCase();
+welcomeMessage = `Welcome to Flatbook, ${currentUser}`;
+welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+let firstInitial = currentUser.slice(0, 1);
+const shortGreeting = `Welcome, ${firstInitial}!`;
